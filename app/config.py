7c76a357d019419816023fcd5638b63a3b7e00a0
Maherimtiyaz <maherimtiyaz7@gmail.com>
2026-02-14 15:38:45 +0530

    Updated JWT token

--- app/config.py
+++ app/config.py
@@ -5,6 +5,7 @@ class Settings(BaseSettings):
     SECRET_KEY: str
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+    jwt_token_prefix: str = "Bearer"
 
     class Config:
         env_file = ".env"
